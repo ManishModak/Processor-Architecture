77a397caa5fa9de64e1c9385e5ff69d96b90fbb3
Manish <111450082+ManishModak@users.noreply.github.com>
2023-04-11 16:04:50 +0530

    Update main.c

--- Assignment No.2/main.c
+++ Assignment No.2/main.c
@@ -1,1 +1,21 @@
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pic18f4550.h>
 
+void main(void)
+{ 
+    int data[] = {0x13,0x11,0x44,0x55} ;
+    
+    int sum = 0;
+    
+    for(int i = 0 ; i < 4 ; i++)
+    {
+        sum = sum + data[i] ;
+    }
+    
+    TRISD = 0;
+    PORTD = sum ;
+    
+    return ;
+}
